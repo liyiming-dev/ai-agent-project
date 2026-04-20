e4c7019ad931598f02fa0828fde3c0024c17ed5e
baiyue <2375627762@qq.com>
2026-04-20 22:16:01 +0800

    第6期-对话历史模块-后端 openapi2ts自动生成后端接口

--- ai-agent-project-frontend/src/api/api/typings.d.ts
+++ ai-agent-project-frontend/src/api/api/typings.d.ts
@@ -86,6 +86,12 @@ declare namespace API {
     message?: string
   }
 
+  type BaseResponsePageChatHistory = {
+    code?: number
+    data?: PageChatHistory
+    message?: string
+  }
+
   type BaseResponsePageUserVO = {
     code?: number
     data?: PageUserVO
@@ -108,6 +114,30 @@ declare namespace API {
     code?: number
     data?: UserVO
     message?: string
+  }
+
+  type ChatHistory = {
+    id?: number
+    message?: string
+    messageType?: string
+    appId?: number
+    userId?: number
+    createTime?: string
+    updateTime?: string
+    isDelete?: number
+  }
+
+  type ChatHistoryQueryRequest = {
+    pageNum?: number
+    pageSize?: number
+    sortField?: string
+    sortOrder?: string
+    id?: number
+    message?: string
+    messageType?: string
+    appId?: number
+    userId?: number
+    lastCreateTime?: string
   }
 
   type chatToGenCodeParams = {
@@ -140,12 +170,22 @@ declare namespace API {
     id: number
   }
 
+  type getInfoParams = {
+    id: number
+  }
+
   type getUserByIdParams = {
     id: number
   }
 
   type getUserVOByIdParams = {
     id: number
+  }
+
+  type listAppChatHistoryParams = {
+    appId: number
+    pageSize?: number
+    lastCreateTime?: string
   }
 
   type LoginUserVO = {
@@ -168,6 +208,19 @@ declare namespace API {
     optimizeCountQuery?: boolean
   }
 
+  type PageChatHistory = {
+    records?: ChatHistory[]
+    pageNumber?: number
+    pageSize?: number
+    totalPage?: number
+    totalRow?: number
+    optimizeCountQuery?: boolean
+  }
+
+  type pageParams = {
+    page: PageChatHistory
+  }
+
   type PageUserVO = {
     records?: UserVO[]
     pageNumber?: number
@@ -175,6 +228,10 @@ declare namespace API {
     totalPage?: number
     totalRow?: number
     optimizeCountQuery?: boolean
+  }
+
+  type removeParams = {
+    id: number
   }
 
   type ServerSentEventString = true
--- ai-agent-project-frontend/src/openapi2ts.config.ts
+++
@@ -1,9 +0,0 @@
-import path from 'node:path'
-
-const openapiConfig = {
-  schemaPath: path.join(__dirname, '..', 'openapi', 'schema.json'),
-  serversPath: './src/api',
-  requestLibPath: '@/request',
-}
-
-export default openapiConfig
